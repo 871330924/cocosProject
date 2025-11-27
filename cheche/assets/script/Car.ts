3fbf1d708546e9ca4ae5b5abe162c45f6eadf94d
sunxiang <871330924@qq.com>
2025-11-28 01:01:42 +0800

    202511280101

--- cheche/assets/script/Car.ts
+++ cheche/assets/script/Car.ts
@@ -1,9 +1,14 @@
-import { _decorator, Component, Input, input, Node, Vec3 } from 'cc';
+import { _decorator, Collider, Component, Input, input, Node, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Car')
 export class Car extends Component {
 
+    //绑定小车的碰撞节点
+    @property(Collider)
+    Player_Collider:Collider = null; //赛车自身的碰撞节点
+
+    //绑定相机节点
     @property(Node)
     Camera_Node :Node = null;
 
@@ -17,12 +22,16 @@ export class Car extends Component {
         //加载函数加载键盘监听
         input.on(Input.EventType.KEY_DOWN,this.Key_Down ,this);
         input.on(Input.EventType.KEY_UP,this.Key_Up ,this);
+
+        //加载碰撞节点
+        this.Player_Collider.on("onTriggerEnter",this.Start_Collider,this); 
     }
 
     protected onDestroy(): void {
         //销毁函数销毁键盘监听
         input.off(Input.EventType.KEY_DOWN,this.Key_Down,this);
         input.off(Input.EventType.KEY_DOWN,this.Key_Up,this);
+        this.Player_Collider.off("onTriggerEnter",this.Start_Collider,this); 
     }
 
     Key_Down(key){
@@ -43,6 +52,10 @@ export class Car extends Component {
         }
     }
 
+    Start_Collider(){
+        console.log("发生碰撞");
+    }
+
 
     start() {
         
@@ -55,15 +68,10 @@ export class Car extends Component {
         //获取小车位置
         const pos = this.node.getPosition();
         //根据按键判断左右移动
-        // if(this.Player_Move.a && !this.Player_Move.d){
-        //     pos.x = pos.x - deltaTime * 10;
-        // }else if(!this.Player_Move.a && this.Player_Move.d){
-        //     pos.x = pos.x + deltaTime * 10;
-        // }
         if(this.Player_Move.a !== this.Player_Move.d){
             pos.x += (this.Player_Move.d ? 1 : -1) * deltaTime * 10;
         }
-
+        pos.x = Math.max(-2.15,Math.min(2.15,pos.x));
         this.node.setPosition(pos.add(new Vec3(0,0,-0.1*DPlayer_Speed)))
         //相机节点位置跟着移动
         //获取相机位置
