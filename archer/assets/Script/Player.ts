5d03134cb81a918156a94e01c043f87f61f1268c
sunxiang <871330924@qq.com>
2025-12-11 18:41:53 +0800

    202512111841

--- archer/assets/Script/Player.ts
+++ archer/assets/Script/Player.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Input, input, instantiate, Node, Prefab, tween, Vec3 } from 'cc';
+import { _decorator, Collider, Collider2D, Component, Contact2DType, Input, input, instantiate, Node, Prefab, tween, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('bazi')
@@ -29,14 +29,28 @@ export class bazi extends Component {
     TOUCH_START(){
         const Arrow_Node = instantiate(this.Arrow_Prefab);//从预制体初始化
         Arrow_Node.setParent(this.Arrow_Parent_Node);//设置父节点
-
+        //开启组件监听
+        Arrow_Node.getComponent(Collider2D).on(Contact2DType.BEGIN_CONTACT,this.Begin_Contact,this);
+        
         //执行缓动逻辑
-        tween(Arrow_Node).to(1,new Vec3(0,620,0)).call(()=>{
+        tween(Arrow_Node).to(1,new Vec3(0,610,0)).call(()=>{
+            this.Arrow_To_Bazi(Arrow_Node);
+            //关闭组件监听
+            Arrow_Node.getComponent(Collider2D).off(Contact2DType.BEGIN_CONTACT,this.Begin_Contact,this);
+        }).start();
+        
+    }
+
+    Begin_Contact(){
+        console.log("碰撞");
+    }
+
+    Arrow_To_Bazi(Arrow_Node:Node){
             const World_Pos = Arrow_Node.getWorldPosition();//获取世界坐标
             Arrow_Node.setParent(this.Bazi);//设置父节点为箭靶节点
             Arrow_Node.setWorldPosition(World_Pos);//重设世界坐标
-
-        }).start();
+            //搞不懂为什么会有这个设置旋转角度的问题
+            Arrow_Node.angle = -this.Bazi.angle;//设置旋转角度
     }
 
     start() {        
